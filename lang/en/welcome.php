7eb28e72cd7a9c1e5f157362e7bccadbfcf80e9d
MichaelKilunga <mickworks30@gmail.com>
2024-09-25 15:10:58 +0300

    Application files

---
+++ lang/en/welcome.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Welcome Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during welcome page for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'welcome_note' => 'Manage Church Administrative Tasks, Members, Events, and Finances.',
+    'password' => 'Neno siri sio sahihi.',
+    'throttle' => 'umejaribu mara nyingi kuingia. Tafadhari jaribu tena baada ya sekunde :.',
+
+];
---
+++ lang/sw/welcome.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Welcome Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during welcome page for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'welcome_note' => 'Usimamizi wa Fedha, Washirika na Matukio ya kanisa.',
+    'welcome_note1' => '.',
+    'password' => 'Neno siri sio sahihi.',
+    'throttle' => 'umejaribu mara nyingi kuingia. Tafadhari jaribu tena baada ya sekunde :.',
+
+];
