5f1b6e458f748d734d93d4f5017982269cd7d59c
Pastor Michael Kilunga <michaelkilunga@localhost.localdomain>
2025-07-24 15:13:44 +0300

    added bishop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,12 +111,13 @@
             <div class="row g-4">
                 @foreach ([
                     ['Super Admin', 'super@mail.com', '0700000000'],
-                    ['Church Admin', 'churchadmin@mail.com', '0711111111'],
-                    ['Church Hof', 'churchhof@mail.com', '0722222222'],
-                    ['Branch Admin', 'branchadmin@mail.com', '0733333333'],
-                    ['Branch Pastor', 'pastor@mail.com', '0744444444'],
-                    ['Branch Head of Finance', 'branchhof@mail.com', '0755555555'],
-                    ['Member', 'member@mail.com', '0766666666'],
+                    ['Church Bishop', 'bishop@mail.com', '0711111111'],
+                    ['Church Admin', 'churchadmin@mail.com', '0722222222'],
+                    ['Church Hof', 'churchhof@mail.com', '0733333333'],
+                    ['Branch Admin', 'branchadmin@mail.com', '0744444444'],
+                    ['Branch Pastor', 'pastor@mail.com', '0755555555'],
+                    ['Branch Head of Finance', 'branchhof@mail.com', '0766666666'],
+                    ['Member', 'member@mail.com', '0777777777'],
                 ] as [$role, $email, $phone])
                     <div class="col-md-4">
                         <div class="card h-100 shadow-sm">
